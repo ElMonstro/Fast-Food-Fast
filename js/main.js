d6d5229c5f98a83e6b0f1a9638687a3498945048
ElMonstro <39956641+ElMonstro@users.noreply.github.com>
2018-09-11 15:18:53 +0300

    Move js folder to root directory

---
+++ js/main.js
@@ -0,0 +1,28 @@
+$('.minus-btn').on('click', function(e){
+    e.preventDefault();
+    var $this = $(this);
+    var $input = $this.closest('div').find('input')
+    var value = parseInt($input.value());
+
+    if (value > 1) {
+        value = value -1;
+    } else {
+        value = 0
+    }
+
+    $input.val(value);
+});
+
+$('plus-button').on('click', function(e){
+    e.preventDefault();
+    var $this = $(this);
+    var $input = $this.closest('div').find('input');
+    var value = parseInt($input.val());
+
+    if (value < 100){
+        value = value + 1;
+    } else {
+        value = 100;
+    }
+    $input.val(value);
+});
