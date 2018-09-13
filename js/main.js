44051861f5766a5a4ac35fb746a2564459ae14eb
ElMonstro <39956641+ElMonstro@users.noreply.github.com>
2018-09-13 18:10:29 +0300

    Add working code for add to tray buttons

--- js/main.js
+++ js/main.js
@@ -3,15 +3,16 @@
 const foodTray = document.querySelector('.food-tray');
 const addButtons = document.querySelectorAll('.button_1');
 
-document.addEventListener('DomContentLoaded', () =>{
+document.addEventListener('DOMContentLoaded', () =>{    
     addButtonClickListener();
 })
 
 
 function addButtonClickListener(){
     for (const addButton of addButtons){
+        console.log(addButton)
         addButton.addEventListener('click', () =>{
-            boxItem = addButton.parentElement.parentElement;
+            const boxItem = addButton.parentElement.parentElement;
             addItemToFoodTray(boxItem);                         
             
         })
@@ -21,27 +22,27 @@ function addButtonClickListener(){
 function addItemToFoodTray(item){
     const itemName = item.querySelector('.name').innerHTML;
     const itemPrice = item.querySelector('.item-price').innerHTML;
-    const itemImg = item.querySelector.image.innerHTML;
+    const itemImg = item.querySelector('.image').innerHTML;
     const itemQuantity = 1;
     const totalPrice = parseInt(itemPrice) * itemQuantity;
 
     const orderDetails =  
-    '<div class="buttons">\
-        <span class="delete-btn"><img src="../static/img/delete-icn.svg" alt=""></span>'+                        
-    '</div>\
+    `<div class="buttons">\
+        <span class="delete-btn"><img src="../static/img/delete-icn.svg" alt=""></span>\                      
+    </div>\
     <div class="cart-image">\
-        ${itemImg}' +                        
-    '</div>\
+        ${itemImg}                        
+    </div>\
     <div class="description">\
         <span>${itemName}</span>\
     </div>\
-    <div class="quantity">' +        
-        '<input type="text" class="qt" value="${itemQuantity}">' +                               
-    '</div>\
-    <div class="total-price"><span>Kshs</span> <span>${totalPrice}</span></div>';
+    <div class="quantity">\        
+        <input type="text" class="qt" value="${itemQuantity}">\                              
+    </div>\
+    <div class="total-price"><span>Kshs</span> <span>${totalPrice}</span></div>`;
 
-    elementToBeAdded = document.createElement('DIV');
-    elementToBeAdded.setAttrbute('class','item');
+    const elementToBeAdded = document.createElement('DIV');
+    elementToBeAdded.className = 'item';
     elementToBeAdded.innerHTML = orderDetails;
 
     foodTray.appendChild(elementToBeAdded);
