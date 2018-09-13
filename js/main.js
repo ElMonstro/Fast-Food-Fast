461f3481524f14d347c846cbf6a2e7b8bae9cf8d
ElMonstro <39956641+ElMonstro@users.noreply.github.com>
2018-09-13 15:54:00 +0300

    Add addItemToFoodTray js function

--- js/main.js
+++ js/main.js
@@ -1,28 +1,41 @@
-$('.minus-btn').on('click', function(e){
-    e.preventDefault();
-    var $this = $(this);
-    var $input = $this.closest('div').find('input')
-    var value = parseInt($input.value());
 
-    if (value > 1) {
-        value = value -1;
-    } else {
-        value = 0
-    }
+const foodTray = document.querySelector('.food-tray');
+const addButtons = document.querySelectorAll('button_1');
 
-    $input.val(value);
-});
+function addButtonClickListener(){
+    for (const addButton of addButtons){
+        addButton.addEventListener('click', () =>{
+            
+            
+        })
+    }        
+}
 
-$('plus-button').on('click', function(e){
-    e.preventDefault();
-    var $this = $(this);
-    var $input = $this.closest('div').find('input');
-    var value = parseInt($input.val());
+function addItemToFoodTray(item){
+    const itemName = item.querySelector('.name').innerHTML;
+    const itemPrice = item.querySelector('.item-price').innerHTML;
+    const itemImg = item.querySelector.image.innerHTML;
+    const itemQuantity = 1;
+    const totalPrice = parseInt(itemPrice) * itemQuantity;
 
-    if (value < 100){
-        value = value + 1;
-    } else {
-        value = 100;
-    }
-    $input.val(value);
-});
+    const orderDetails =  
+    '<div class="buttons">\
+        <span class="delete-btn"><img src="../static/img/delete-icn.svg" alt=""></span>'+                        
+    '</div>\
+    <div class="cart-image">\
+        ${itemImg}' +                        
+    '</div>\
+    <div class="description">\
+        <span>${itemName}</span>\
+    </div>\
+    <div class="quantity">' +        
+        '<input type="text" class="qt" value="${itemQuantity}">' +                               
+    '</div>\
+    <div class="total-price"><span>Kshs</span> <span>${totalPrice}</span></div>';
+
+    elementToBeAdded = document.createElement('DIV');
+    elementToBeAdded.setAttrbute('class','item');
+    elementToBeAdded.innerHTML = orderDetails;
+
+    foodTray.appendChild(elementToBeAdded);
+}
