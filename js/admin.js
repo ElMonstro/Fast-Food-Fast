b26571eb1163798b51b722cdda7f26e1fd7112a7
unknown <jratcher@gmail.com>
2018-09-25 13:18:41 +0300

    Setup admin page javascript and orders object

---
+++ js/admin.js
@@ -0,0 +1,10 @@
+'use strict';
+var orders = 
+{jay: [['coke', 1,],['fries', 2,]],
+moe:[['sausage', 4],['samosa',3]],
+emm: [['burger', 3],['coke', 5],['sausage', 2]],
+fiddy: [['coke', 1],['fries']]};
+
+function displayOrders(){ 
+    
+}
