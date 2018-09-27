741d03a1857b69a9b75d03c7794c1a84ae438e69
unknown <jratcher@gmail.com>
2018-09-27 17:28:47 +0300

    Fix markup and styling, add icons

--- js/admin.js
+++ js/admin.js
@@ -1,10 +1,10 @@
 'use strict';
-var orders = 
+var newOrders = 
 {jay: [['coke', 1,],['fries', 2,]],
 moe:[['sausage', 4],['samosa',3]],
 emm: [['burger', 3],['coke', 5],['sausage', 2]],
 fiddy: [['coke', 1],['fries']]};
 
 function displayOrders(){ 
-    
+        
 }
